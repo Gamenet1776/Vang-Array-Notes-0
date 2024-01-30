eb388eb701727dfbd11d29992a2dc502fdcf7afd
Gamenet1776 <143832472+Gamenet1776@users.noreply.github.com>
2024-01-30 08:59:48 -0800

    complete

---
+++ .js
@@ -0,0 +1,26 @@
+// Organizing and storing data is a foundational concept. 
+let newYearsResolutions = ['Keep a journal', 'Take a Falconry Class', 'Learn Kubernetes'];
+newYearsResolutions [0] = 'eat';
+newYearsResolutions [1] = 'sleep';
+newYearsResolutions [2] = 'wake up';
+console.log(newYearsResolutions)
+
+/* Array can hold any type of data, for example another array, each element in an array has a numbered posistion
+known as an index, arrays in JavaScript are zero-indexted, postion start counting
+from 0 */
+
+const hobbies = [ 'Draw', 'Piano','Gaming' ];
+console.log( hobbies [0] )
+
+let cities = [ 'Draw', 'Piano','Gaming' ];
+hobbies [cities[0]]
+
+listitem = ['Draw', 'Piano','Gaming'];
+console.log(listitem[3])
+
+let seasons = ['winter', 'Spring', 'Summer', 'Fall'];
+
+seasons[3] = 'Autumn';
+
+console.log(seasons);
+
